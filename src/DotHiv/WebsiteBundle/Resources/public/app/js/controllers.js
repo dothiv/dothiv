1a724510db232351ca5a1c5d339b5e79ae0a2de4
Nils Wisiol <nils.wisiol@dothiv.org>
2013-05-29 13:34:26 +0200

    configured symfony and angular to use the HTTP auth interceptor to handle HTTP 401 events.

--- src/DotHiv/WebsiteBundle/Resources/public/app/js/controllers.js
+++ src/DotHiv/WebsiteBundle/Resources/public/app/js/controllers.js
@@ -2,10 +2,51 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', ['http-auth-interceptor', 'ui.bootstrap']).
+    controller('MyCtrl1', ['$scope', '$http', 'authService', '$dialog', function($scope, $http, authService, $dialog) {
+        $scope.$on('event:auth-loginRequired', function() {
+            console.log("login required.");
+            
+            // Thats how it should be like:
+            // show a login form and then perform the login
+//            var opts = {
+//                    backdrop: true,
+//                    keyboard: true,
+//                    backdropClick: true,
+//                    template: '<h1>use templates!</h1>',
+//            };
+//            
+//            $dialog.dialog(opts).open();
+            
+            console.log("sending login data ...");
+            $http({
+                method: 'POST',
+                url: '/app_dev.php/login_check',
+                data: '_username=nils&_password=test', // hard coded login data
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function() {
+                console.log("login successful.");
+                authService.loginConfirmed();
+            });
+          });
+        $scope.$on('event:auth-loginConfirmed', function() {
+            console.log('login confirmed');
+          });
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+        $scope.nav = function() {
+            $http.get('/app_dev.php/api/doc/').success(function(){
+                // not executed before user is authenticated
+                console.log("resumed http request successful completed.");
+            });
+        };
+        
+        $scope.logout = function() {
+            console.log("logging out ...");
+            $http.get('/app_dev.php/logout').success(function() {
+                console.log("logout complete.");
+            });
+        }
+    }])
+    .controller('MyCtrl2', [function() {
+    
+    }]);
