2d4bbe1b6af40690554d9d71f978fc8c4852374b
Benedikt Budig <bb@dothiv.org>
2013-05-29 17:50:36 +0200

    Class for static sites improved.

--- src/DotHiv/BusinessBundle/Entity/Website/StaticSite.php
+++ src/DotHiv/BusinessBundle/Entity/Website/StaticSite.php
@@ -5,14 +5,15 @@ namespace DotHiv\BusinessBundle\Entity;
 use Gedmo\Translatable\Translatable;
 
 /**
+ * This class holds the content for a static website.
+ * 
  * @ORM\Entity
  * @Serializer\ExclusionPolicy("all")
  */
-class StaticSide extends Entity implements Translatable
+class StaticSite extends Entity implements Translatable
 {
-
     /**
-     * name of the static side, as referred to in its URL
+     * name of the static side
      *
      * @Assert\NotBlank
      * @ORM\Column(type="string",length=255)
@@ -24,7 +25,6 @@ class StaticSide extends Entity implements Translatable
      * content of the static side
      *
      * @Gedmo\Translatable
-     * @Assert\NotBlank
      * @ORM\Column(type="text")
      * @Serializer\Expose
      */
@@ -35,15 +35,36 @@ class StaticSide extends Entity implements Translatable
      */
     private $locale;
 
-
+    /**
+     * This function returns the name of the static site.
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * This function updates the name of the static site to the given value.
+     */
     public function setName($name)
     {
         $this->name = $name;
+    }
+
+    /**
+     * This function returns the content of the static site.
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * This function updates the content of the static site to the given value.
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
     }
 
     public function setTranslatableLocale($locale)
