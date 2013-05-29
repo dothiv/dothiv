305c14b8ff1a4856bbfc854c57427f8225cabfe8
Benedikt Budig <bb@dothiv.org>
2013-05-29 09:32:44 +0200

    Hashbanging for AngularJS URLs enabled. This is necessary for search engine indexing.

--- src/DotHiv/WebsiteBundle/Resources/public/app/js/app.js
+++ src/DotHiv/WebsiteBundle/Resources/public/app/js/app.js
@@ -7,4 +7,8 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/view1', {templateUrl: '/bundles/website/app/partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: '/bundles/website/app/partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/view1'});
+  }]).
+  config(['$locationProvider', function($location) {
+    $location.hashPrefix('!');
   }]);
+
