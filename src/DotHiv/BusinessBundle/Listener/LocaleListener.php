4245285a9e4447f4a2256a750f6cc92ed459a134
Nils Wisiol <nils.wisiol@dothiv.org>
2013-06-01 13:23:53 +0200

    added proof of concept for translation of static texts (for now, WebsiteChartiyBundle only). The user's locale is determined by the Accept-Language header only. A language chooser is intended to be implemented by the angular SPA.

---
+++ src/DotHiv/BusinessBundle/Listener/LocaleListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DotHiv\BusinessBundle\Listener;
+
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+/**
+ * Sets the user's locale by the Language-Accept header the user sends.
+ * 
+ * @author Nils Wisiol <mail@nils-wisiol.de>
+ *
+ */
+class LocaleListener implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(Container $container) {
+        $this->container = $container;
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $req = $event->getRequest();
+        $locale = $req->getPreferredLanguage();
+        $req->setLocale($locale);
+    }
+}
