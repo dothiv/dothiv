d993a687763165c180c46edbb16604a515cf4313
Nils Wisiol <nils.wisiol@dothiv.org>
2013-06-04 15:01:30 +0200

    validation of registration form

--- src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/directives.js
+++ src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/directives.js
@@ -4,8 +4,24 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    directive("repeatPassword", function() {
+        return {
+            require: "ngModel",
+            link: function(scope, elem, attrs, ctrl) {
+                var otherInput = elem.inheritedData("$formController")[attrs.repeatPassword];
+
+                ctrl.$parsers.push(function(value) {
+                    if(value === otherInput.$viewValue) {
+                        ctrl.$setValidity("repeat", true);
+                        return value;
+                    }
+                    ctrl.$setValidity("repeat", false);
+                });
+
+                otherInput.$parsers.push(function(value) {
+                    ctrl.$setValidity("repeat", value === ctrl.$viewValue);
+                    return value;
+                });
+            }
+        };
+    });
