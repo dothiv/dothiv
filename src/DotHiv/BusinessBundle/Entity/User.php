a64221d99f96a5206ad286f885868900159d13ac
Benedikt Budig <bb@dothiv.org>
2013-06-05 17:22:43 +0200

    JMSSerializer configuration moved from annotations to yml files.

--- src/DotHiv/BusinessBundle/Entity/User.php
+++ src/DotHiv/BusinessBundle/Entity/User.php
@@ -6,6 +6,7 @@ use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @ORM\Entity
