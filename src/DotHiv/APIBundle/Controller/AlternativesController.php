2d6e2a8f524b5811fdce9a41c06533ff288c2cc1
Nils Wisiol <nils.wisiol@dothiv.org>
2013-06-06 18:49:21 +0200

    Added API endpoint for lookup of .hiv alternatives for domain names: /api/alternatives?q=google.de

---
+++ src/DotHiv/APIBundle/Controller/AlternativesController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace DotHiv\APIBundle\Controller;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\Query\Expr;
+use DotHiv\BusinessBundle\Entity\DomainList;
+use DotHiv\BusinessBundle\Form\DomainListType;
+use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+/**
+ * API endpoint for getting .hiv alternatives.
+ * See BusinessBundle\Entity\DomainAlternative.
+ * 
+ * @author Nils Wisiol <mail@nils-wisiol.de>
+ */
+class AlternativesController extends FOSRestController {
+
+    /**
+     * Returns a list of .hiv domain name alternatives for the given set of domains. 
+     * 
+     * @ApiDoc(
+     *   section="alternatives",
+     *   resource=true,
+     *   description="Find alternatives for a given set of domain names.",
+     *   statusCodes={
+     *     200="Returned when successful",
+     *   },
+     *   filters={
+     *       {"name"="q", "description"="Query for domain names", "dataType"="comma seperated list of domain names"}
+     *   }
+     * )
+     */
+    public function getAlternativesAction() {
+        $domainList = explode(',', $this->getRequest()->get('q'));
+
+        if (count($domainList) == 0) 
+            throw new HttpException(404);
+
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository('DotHivBusinessBundle:DomainAlternative')->createQueryBuilder('d');
+        $result = $qb
+            ->where($qb->expr()->in('d.domain', $domainList))
+            ->andWhere('d.trusted = true')
+            ->getQuery()
+            ->getResult();
+
+        return $result;
+    }
+
+}
---
+++ src/DotHiv/BusinessBundle/Entity/Domain.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DotHiv\BusinessBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * Represents a registered .hiv-Domain.
+ * 
+ * @ORM\Entity
+ * @Serializer\ExclusionPolicy("all")
+ * 
+ * @author Nils Wisiol <mail@nils-wisiol.de>
+ */
+class Domain extends Entity {
+
+    /**
+     * FQDN, no trailing dot.
+     * 
+     * @ORM\Column(type="string",length=255)
+     * @Assert\Regex("/^[^.]{1,67}\.hiv$/")
+     * @Serializer\Expose
+     */
+    protected $name;
+
+    /**
+     * A list of domains that offer equivalent or similiar content.
+     * @ORM\OneToMany(targetEntity="DomainAlternative",mappedBy="hivDomain")
+     */
+    protected $alternatives;
+
+}
