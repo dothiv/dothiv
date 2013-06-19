6038bca92548f15ed41d44d5f65a3284ac4389a9
Nils Wisiol <nils.wisiol@dothiv.org>
2013-06-19 11:32:00 +0200

    created a shortcut-directive for building form fields with label and tooltip; for a example see login/registration form

--- src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/directives.js
+++ src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/directives.js
@@ -3,8 +3,8 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-    directive("repeatPassword", function() {
+angular.module('myApp.directives', [])
+    .directive("repeatPassword", function() {
         return {
             require: "ngModel",
             link: function(scope, elem, attrs, ctrl) {
@@ -22,6 +22,75 @@ angular.module('myApp.directives', []).
                     ctrl.$setValidity("repeat", value === ctrl.$viewValue);
                     return value;
                 });
+            }
+        };
+    })
+    .directive("dhInput", function($compile) {
+        return {
+            restrict: 'E',
+            scope: {
+                    // core input attributes
+                    name: '@dhName',
+                    placeholder: '@dhPlaceholder',
+                    ngModel: '=',
+                    Required: '@dhRequired',
+                    Autofocus: '@dhAutofocus',
+
+                    // label
+                    label: '@dhLabel',
+
+                    // tooltip
+                    tooltip: '@dhTooltip',
+                    tooltipPlacement: '@dhTooltipPlacement',
+                    tooltipTrigger: '@dhTooltipTrigger',
+                    tooltipInvalid: '@dhTooltipInvalid',
+                    tooltipShowInvalid: '=dhTooltipShowInvalid',
+                },
+            template: '<div>' +
+                        '<label for="[[ name ]]" ng-transclude>[[ label ]]</label>' +
+                        '<input ' +
+                            // core input attributes
+                            'name="[[ name ]]"' +
+                            'id="[[ name ]]" ' +
+                            'placeholder="[[ placeholder ]]"' +
+                            'ng-model="$parent.ngModel"' +
+
+                            // tooltip
+                            'tooltip="[[ tooltiptext() ]]" ' +
+                            'tooltip-placement="[[ tooltipPlacement ]]" ' +
+                            'tooltip-trigger="[[ tooltipTrigger ]]" ' +
+                            '/>' +
+                      '</div>',
+            replace: true,
+            transclude: true,
+            require: 'ngModel',
+            priority: 10,
+            controller: ['$scope', '$element', '$attrs', '$transclude', function($scope, $element, $attrs, $transclude) {
+                $scope.tooltiptext = function() {
+                    console.log($scope.tooltipShowInvalid ? 'valid' : 'invalid');
+                    return $scope.tooltipShowInvalid ? $scope.tooltipInvalid : $scope.tooltip;
+                }
+            }],
+            compile: function(tElement, tAttrs, transclude) {
+                var input = tElement.find('input');
+
+                // move css classes
+                input.attr('class', tElement.attr('class'));
+                tElement.removeAttr('class');
+
+                // move type
+                input.attr('type', tElement.attr('type'));
+                tElement.removeAttr('type');
+
+                // require attribute
+                if (tElement.attr("dh-required") != undefined) {
+                    input.prop("required", true);
+                }
+
+                // autofocus attribute
+                if (tElement.attr("dh-autofocus") != undefined) {
+                    input.prop("autofocus", true);
+                }
             }
         };
     });
