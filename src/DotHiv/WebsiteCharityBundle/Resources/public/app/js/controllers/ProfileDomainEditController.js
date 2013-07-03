b137dda82b6b5186e290c58218955aa6c81d3095
Benedikt Budig <bb@dothiv.org>
2013-07-03 16:10:44 +0200

    Improved display of forms. Work on validation in 'domain edit' started.

--- src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/controllers/ProfileDomainEditController.js
+++ src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/controllers/ProfileDomainEditController.js
@@ -2,12 +2,26 @@
 
 angular.module('dotHIVApp.controllers').controller('ProfileDomainEditController', ['$scope', '$location', '$stateParams', 'security',
     function($scope, $location, $stateParams, security) {
+
         $scope.domainName = $stateParams.domainName; //TODO
+
         $scope.domainforwarding = 'true';
         $scope.bannerposition = 'center';
         $scope.bannersecondposition = 'upper';
+        $scope.targetdomain = '';
         $scope.language = 'Deutsch';
         $scope.languages = {'german':'Deutsch', 'english':'Englisch', 'spanish':'Spanisch', 'latin':'Latein'};
+
+        // form configuration
         $scope.formclean = true;
+        $scope.nextStep = function (form, tab) {
+            if (form.$valid) {
+                console.log('valid');
+                tab.active = true;
+            } else {
+                console.log('invalid');
+                $scope.formclean = false;
+            }
+        };
     }
 ]);
