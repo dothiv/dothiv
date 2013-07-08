40e44f82bff3cffa386592828d94480f6bb8c6e4
Benedikt Budig <bb@dothiv.org>
2013-07-08 14:47:14 +0200

    'Domain' model in angular app now supports 'banners' sub resource

--- src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/services/models/domain.js
+++ src/DotHiv/WebsiteCharityBundle/Resources/public/app/js/services/models/domain.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('dotHIVApp.services').factory('dothivDomainResource', function($resource, dothivResourceDefaultActions) {
-    return $resource('/app_dev.php/api/domains/:claims/:id', {}, {
-        'get':    {method:'GET', params: {id:'@id'}},
-        'query':  {method:'GET', isArray:true},
-        'search': {method:'GET', params: {token:'@token'}},
-        'claim':  {method:'POST', params: {claims:'claims'}},
-        'save':   {method:'POST', params: {id:'@id'}},
+    return $resource('/app_dev.php/api/domains/:claims/:id/:sub', {}, {
+        'get':        {method:'GET', params: {id:'@id'}},
+        'query':      {method:'GET', isArray:true},
+        'search':     {method:'GET', params: {token:'@token'}},
+        'getBanners': {method:'GET', isArray:true, params: {id:'@id', sub:'banners'}},
+        'claim':      {method:'POST', params: {claims:'claims'}},
+        'save':       {method:'POST', params: {id:'@id'}},
     });
 });
