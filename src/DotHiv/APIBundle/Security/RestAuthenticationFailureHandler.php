717a19e88d2da9be4d6ca76544dd1519a5c16571
Nils Wisiol <nils.wisiol@dothiv.org>
2013-07-12 14:55:18 +0200

    using a template for failed Facebook login error message

--- src/DotHiv/APIBundle/Security/RestAuthenticationFailureHandler.php
+++ src/DotHiv/APIBundle/Security/RestAuthenticationFailureHandler.php
@@ -20,6 +20,7 @@ use Symfony\Component\Security\Http\HttpUtils;
 class RestAuthenticationFailureHandler extends DefaultAuthenticationFailureHandler {
 
     private $translator;
+    private $templating;
 
     /**
      * @param HttpKernelInterface $httpKernel
@@ -28,14 +29,16 @@ class RestAuthenticationFailureHandler extends DefaultAuthenticationFailureHandl
      * @param LoggerInterface $logger
      * @param TranslatorInterface $translator
      */
-    public function __construct(HttpKernelInterface $httpKernel, HttpUtils $httpUtils, array $options, LoggerInterface $logger = null, TranslatorInterface $translator = null)
+    public function __construct(HttpKernelInterface $httpKernel, HttpUtils $httpUtils, array $options, LoggerInterface $logger = null, TranslatorInterface $translator = null, $templating = null)
     {
         $this->translator = $translator;
+        $this->templating = $templating;
         parent::__construct($httpKernel, $httpUtils, $options, $logger);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception) {
         $msg = ''; $status = 500;
+        $template = '';
         if ($exception instanceof \Symfony\Component\Security\Core\Exception\SessionUnavailableException) {
             $msg = 'security.login.failure.session';
             $status = 400;
@@ -45,15 +48,20 @@ class RestAuthenticationFailureHandler extends DefaultAuthenticationFailureHandl
         } elseif ($exception instanceof \DotHiv\BusinessBundle\Security\Exception\DuplicateEmailAddressException) {
             $msg = 'security.login.facebook.failure.duplicate';
             $status = 200; // send 200 OK though its a client error, as this is a classic browser request, but not an RESTful API call
+            $template = 'Security:FacebookFailure.html.twig';
         } else {
             $msg = 'security.login.failure.servererror';
             $status = 500;
         }
 
-        if ($this->translator != null) {
-            $msg = $this->translator->trans($msg);
+        if ($this->templating != null && $template) {
+            $response = $this->templating->renderResponse('DotHivAPIBundle:' . $template, array('msg' => $msg));
+        } else {
+            if ($this->translator != null) {
+                $msg = $this->translator->trans($msg);
+            }
+            $response = new Response($msg);
         }
-        $response = new Response($msg);
         $response->setStatusCode($status);
         return $response;
     }
