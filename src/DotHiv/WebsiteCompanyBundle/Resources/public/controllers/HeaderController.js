89e2834de06102bb0330e62c153846feea4497be
Andre Loeffler <info@andre-loeffler.net>
2013-07-24 12:46:54 +0200

    feat(company): index.html of CompanyBundle
    main page for companyBundle views (header, footer, controller, css).
    found some CSS-code that could be moved to BaseBundle

--- src/DotHiv/WebsiteCompanyBundle/Resources/public/controllers/HeaderController.js
+++ src/DotHiv/WebsiteCompanyBundle/Resources/public/controllers/HeaderController.js
@@ -1,8 +1,21 @@
 'use strict';
 
-angular.module('dotHIVApp.controllers').controller('HeaderController', ['$scope', 
-    function($scope) {
-    
+angular.module('dotHIVApp.controllers').controller('HeaderController', ['$scope', 'locale',
+    function($scope, locale) {
+        $scope.locale = locale;
+        $scope.siteLanguages = {
+                                'de': 'Deutsch',
+                                'en': 'English',
+                                'key': 'Keys only'
+                               };
+        
+        $scope.$watch('locale.language', function() {
+            locale.set(locale.language);
+        });
+        
+        $scope.$on('localeInitialized', function() {
+            $scope.finishedbooting = true;
+        });
     }
 ]);
 
