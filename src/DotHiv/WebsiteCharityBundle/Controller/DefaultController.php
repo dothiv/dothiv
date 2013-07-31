73c2bc54c9980785ac5a27590a71ca696120e583
Moritz Bellach <moritzbellach@gmail.com>
2013-07-31 14:32:51 +0200

    feat(google bot switch): finally got the switch and render from db right :)

--- src/DotHiv/WebsiteCharityBundle/Controller/DefaultController.php
+++ src/DotHiv/WebsiteCharityBundle/Controller/DefaultController.php
@@ -4,24 +4,28 @@ namespace DotHiv\WebsiteCharityBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         // get request
-        $request = $this->getRequest();
-
-	$url1 = $request->getUri();
-	$url2 = preg_replace('/\?_escaped_fragment_=/', '#!', $url1);
-
-	if($url1 === $url2){
+	$request = $this->getRequest();
+	if($request->query->has('_escaped_fragment_') === false){
 		// we go ahead and return the regular index site with our angular app
 		return $this->render('DotHivWebsiteCharityBundle:Default:index.html.twig');
 	} else {
 		// we need to return a pre-rendered version of the requested site
-		// TODO: return dom from db
-		return new Response("gbot_switch test. url: " . $url1 . " Locale:  " . $request->getPreferredLanguage());
+		$domain = $request->getHttpHost();
+		$fragment = $request->get('_escaped_fragment_');
+
+		$em = $this->getDoctrine()->getManager();
+		$pages = $em->getRepository('DotHivBusinessBundle:Crawler\Page')->findBy(array('host'=>$domain, 'fragment'=>$fragment));
+		if(sizeof($pages) == 0){
+			throw new HttpException(404, 'did not find page for domain ' . $domain . ' and fragment ' . $fragment . ' in db');
+		}
+		return new Response(stream_get_contents($pages[0]->getDom()));
 	}
     }
 }
