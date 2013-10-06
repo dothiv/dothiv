8654d1476c96e645cf9408fed7906602a9252dba
Nils Wisiol <nils.wisiol@dothiv.org>
2013-10-06 13:51:23 +0200

    chore(Symfony): updated requirements

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Gregwar\FormBundle\GregwarFormBundle(),
             new DotHiv\BusinessBundle\DotHivBusinessBundle(),
             new DotHiv\APIBundle\DotHivAPIBundle(),
             new DotHiv\WebsiteCharityBundle\DotHivWebsiteCharityBundle(),
