d7b59e5e866e0b8ca2c8b2f669bf2e608da133d2
Nils Wisiol <nils.wisiol@dothiv.org>
2013-10-11 14:45:44 +0200

    feat(Registration): cli for the registration service

---
+++ src/DotHiv/BusinessBundle/Command/RegistrationTransferredCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DotHiv\BusinessBundle\Command;
+
+use DotHiv\BusinessBundle\Service\Registration;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * This command can be used to declare a domain to be transferred.
+ *
+ * @author Nils Wisiol <mail@nils-wisiol.de>
+ */
+class RegistrationTransferredCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this
+        ->setName('dothiv:registration:transferred')
+        ->setDescription('Take action for a transferred domain.')
+        ->addArgument('name', InputArgument::REQUIRED, 'The name of the transferred domain.')
+        ->addArgument('email', InputArgument::REQUIRED, 'The email address of the new registrant.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $reg = $this->getContainer()->get('registration');
+        $domain = $reg->transferred($input->getArgument('name'), $input->getArgument('email'));
+        $output->writeln('Domain ' . $domain->getName() . ' transferred. New token: ' . $domain->getClaimingToken());
+
+        // clear mail spool, see http://symfony.com/doc/2.0/cookbook/console/sending_emails.html
+        $this->getContainer()->get('mailer')->getTransport()->getSpool()->flushQueue($this->getContainer()->get('swiftmailer.transport.real'));
+    }
+
+}
