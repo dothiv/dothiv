2a8b10714319bd284a14a05380084d6f5765f7ab
Nils Wisiol <nils.wisiol@dothiv.org>
2013-10-18 13:36:49 +0200

    feat(release): removed unneccessary features from the UI

--- src/DotHiv/WebsiteCharityBundle/Resources/public/controllers/ProfileSummaryController.js
+++ src/DotHiv/WebsiteCharityBundle/Resources/public/controllers/ProfileSummaryController.js
@@ -7,16 +7,16 @@ angular.module('dotHIVApp.controllers').controller('ProfileSummaryController', [
             {"username": security.state.user.username}
         );
 
-        // TODO get personal list of projects from server
-        $scope.projects = [ {name: 'Awesome project', votes: 3268}, {name: 'Lame project', votes: 12}, {name: 'Nils\'s project', votes: 122626} ];
-        $scope.projects.$resolved = true; // workaround for the loader
-
-        // TODO get personal list of votes from server
-        $scope.votes = [ {}, {}, {}, {}, {} ];
-        $scope.votes.$resolved = true; // workaround for the loader
-
-        // TODO get personal list of comments from server
-        $scope.comments = [ {project: 'Some project', text: 'Your project is really awesome!'}, {project: 'Some other project', text: 'This one really sucks.'} ];
-        $scope.comments.$resolved = true; // workaround for the loader
+        // // TODO get personal list of projects from server
+        // $scope.projects = [ {name: 'Awesome project', votes: 3268}, {name: 'Lame project', votes: 12}, {name: 'Nils\'s project', votes: 122626} ];
+        // $scope.projects.$resolved = true; // workaround for the loader
+ 
+        // // TODO get personal list of votes from server
+        // $scope.votes = [ {}, {}, {}, {}, {} ];
+        // $scope.votes.$resolved = true; // workaround for the loader
+ 
+        // // TODO get personal list of comments from server
+        // $scope.comments = [ {project: 'Some project', text: 'Your project is really awesome!'}, {project: 'Some other project', text: 'This one really sucks.'} ];
+        // $scope.comments.$resolved = true; // workaround for the loader
     }
 ]);
