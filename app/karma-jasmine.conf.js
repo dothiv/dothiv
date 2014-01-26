dffba37d7ec48f46d0f5387e89bfee788bd4fce6
Markus Tacker <m@coderbyheart.de>
2014-01-26 15:21:58 +0100

    fix(testing-karma): run angular unit tests

--- app/karma-jasmine.conf.js
+++ app/karma-jasmine.conf.js
@@ -2,31 +2,24 @@ module.exports = function (config) {
     config.set({
 
         // base path, that will be used to resolve files and exclude
-        basePath: '../src/DotHiv',
+        basePath: '../',
 
         // frameworks to use
-        frameworks: ['jasmine', 'ng-scenario'],
+        frameworks: ['jasmine'],
 
 
         // list of files / patterns to load in the browser
         files: [
+            // Angular and other vendor stuff
+            'src/DotHiv/WebsiteBaseBundle/Resources/public/vendor/angular/angular.js',
+            'src/DotHiv/WebsiteBaseBundle/Resources/public/vendor/angular/angular-*.js',
+            'src/DotHiv/WebsiteBaseBundle/Resources/test/lib/angular/angular-mocks.js',
+            'src/DotHiv/WebsiteBaseBundle/Resources/public/vendor/angular-translate.js',
+            'src/DotHiv/WebsiteBaseBundle/Resources/public/vendor/**/*.js',
             // WebsiteBaseBundle
-            'WebsiteBaseBundle/Resources/public/vendor/angular/angular.js',
-            'WebsiteBaseBundle/Resources/public/vendor/angular/angular-*.js',
-            'WebsiteBaseBundle/Resources/test/lib/angular/angular-mocks.js',
-            'WebsiteBaseBundle/Resources/public/vendor/angular-translate.js',
-            'WebsiteBaseBundle/Resources/public/vendor/**/*.js',
-            'WebsiteBaseBundle/Resources/test/test.js',
-            'WebsiteBaseBundle/Resources/public/**/*.js',
-            'WebsiteBaseBundle/Resources/test/unit/**/*.js',
-            // WebsiteCharityBundle
-            'WebsiteCharityBundle/Resources/public/app.js',
-            'WebsiteCharityBundle/Resources/public/**/*.js',
-            'WebsiteCharityBundle/Resources/test/e2e/**/*.js',
-            // WebsiteCompanyBundle
-            'WebsiteCompanyBundle/Resources/public/app.js',
-            'WebsiteCompanyBundle/Resources/public/**/*.js',
-            'WebsiteCompanyBundle/Resources/test/e2e/**/*.js'
+            'src/DotHiv/WebsiteBaseBundle/Resources/test/test.js',
+            'src/DotHiv/WebsiteBaseBundle/Resources/public/**/*.js',
+            'src/DotHiv/WebsiteBaseBundle/Resources/test/unit/**/*.js'
         ],
 
         // list of files to exclude
@@ -76,7 +69,7 @@ module.exports = function (config) {
         // if true, it capture browsers, run tests and exit
         singleRun: true,
         junitReporter: {
-            outputFile: '../../build/logs/karma-results.xml'
+            outputFile: 'build/logs/karma-results.xml'
         }
     });
 };
