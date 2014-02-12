2c3929f688a7988470f66d712ff52d56d41d85f9
Markus Tacker <m@coderbyheart.de>
2014-02-12 19:32:50 +0100

    style(maintenance.php): move return statement into function

--- web/maintenance.php
+++ web/maintenance.php
@@ -26,12 +26,12 @@
   <p><small>This page will reload after <?php echo $retry; ?> seconds.</small></p>
   <script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.min.js"></script>
   <script type="text/javascript">
-    if (!window.location.hash.length > 0) return; 
     $(function() {
-	// Append hashBang if present.
-	var refreshMeta = $('head meta[http-equiv="refresh"]');
-  	var refreshUrl = refreshMeta.prop('content').split(';')[1].trim().split('=')[1];
-  	refreshMeta.prop('content', refreshMeta.prop('content').replace(refreshUrl, refreshUrl + "/" +  window.location.hash));
+        if (!window.location.hash.length > 0) return;
+        // Append hashBang if present.
+        var refreshMeta = $('head meta[http-equiv="refresh"]');
+        var refreshUrl = refreshMeta.prop('content').split(';')[1].trim().split('=')[1];
+        refreshMeta.prop('content', refreshMeta.prop('content').replace(refreshUrl, refreshUrl + "/" +  window.location.hash));
     });
   </script>
 </body>
