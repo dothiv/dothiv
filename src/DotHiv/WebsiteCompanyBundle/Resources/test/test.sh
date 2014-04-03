46b5d63458036af89a6901f138704ba7c652127b
Markus Tacker <m@coderbyheart.de>
2014-04-03 11:47:15 +0200

    fix(karma): infer karma and chrome bin from env

--- src/DotHiv/WebsiteCompanyBundle/Resources/test/test.sh
+++ src/DotHiv/WebsiteCompanyBundle/Resources/test/test.sh
@@ -6,4 +6,6 @@ echo ""
 echo "Starting Karma Server (http://karma-runner.github.io)"
 echo "-------------------------------------------------------------------"
 
-CHROME_BIN=`which chromium-browser` karma start $BASE_DIR/config/karma.conf.js $*
+CHROME_BIN=${CHROME_BIN:=`which chromium-browser`}
+KARMA_BIN=${KARMA_BIN:=karma}
+$KARMA_BIN start $BASE_DIR/config/karma.conf.js $*
