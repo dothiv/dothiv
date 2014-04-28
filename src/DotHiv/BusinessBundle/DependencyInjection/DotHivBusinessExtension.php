16aa534add210afa13a0c48f6a4c9a8b346efbb9
Markus Tacker <m@coderbyheart.de>
2014-04-28 16:18:53 +0200

    feat(banner): add command to update banner config

--- src/DotHiv/BusinessBundle/DependencyInjection/DotHivBusinessExtension.php
+++ src/DotHiv/BusinessBundle/DependencyInjection/DotHivBusinessExtension.php
@@ -25,6 +25,7 @@ class DotHivBusinessExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('repositories.yml');
         $loader->load('validators.yml');
     }
 }
---
+++ src/DotHiv/BusinessBundle/Tests/Command/ClickCounterConfigureCommandTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace DotHiv\BusinessBundle\Tests\Entity\Command;
+
+use DotHiv\BusinessBundle\Command\ClickCounterConfigureCommand;
+use DotHiv\BusinessBundle\Entity\Banner;
+use DotHiv\BusinessBundle\Entity\Domain;
+use DotHiv\BusinessBundle\Service\ClickCounterConfig;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Test for ClickCounterConfigureCommand.
+ *
+ * @author    Markus Tacker <m@dotHIV.org>
+ */
+class ClickCounterConfigureCommandTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Symfony\Component\Console\Input\InputInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockInput;
+
+    /**
+     * @var \Symfony\Component\Console\Output\OutputInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockOutput;
+
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockContainer;
+
+    /**
+     * @var ClickCounterConfig|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockClickCounterConfig;
+
+    /**
+     * @var \Doctrine\ORM\EntityRepository|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockBannerRepo;
+
+    /**
+     * @test
+     * @group DotHIVBusinessBundle
+     * @group Command
+     */
+    public function itShouldBeInstantiateable()
+    {
+        $this->assertInstanceOf('DotHiv\BusinessBundle\Command\ClickCounterConfigureCommand', $this->getTestObject());
+    }
+
+    /**
+     * @test
+     * @group   DotHIVBusinessBundle
+     * @group   Command
+     * @depends itShouldBeInstantiateable
+     */
+    public function itShouldConfigureBanners()
+    {
+        $domain = new Domain();
+        $domain->setName('stop.hiv');
+        $banner = new Banner();
+        $banner->setDomain($domain);
+
+        $containerMap = array(
+            array('clickcounter', ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE, $this->mockClickCounterConfig),
+            array('dothiv.repository.banner', ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE, $this->mockBannerRepo),
+        );
+        $this->mockContainer->expects($this->any())->method('get')
+            ->will($this->returnValueMap($containerMap));
+
+        $this->mockBannerRepo->expects($this->once())->method('findAll')
+            ->will($this->returnValue(array($banner)));
+
+        $this->mockClickCounterConfig->expects($this->once())->method('setup')
+            ->with($domain, $banner);
+
+        $this->assertEquals(0, $this->getTestObject()->run($this->mockInput, $this->mockOutput));
+    }
+
+    /**
+     * @return ClickCounterConfigureCommand
+     */
+    protected function getTestObject()
+    {
+        $command = new ClickCounterConfigureCommand();
+        $command->setContainer($this->mockContainer);
+        return $command;
+    }
+
+    /**
+     * Test setup
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->mockInput = $this->getMockBuilder('\Symfony\Component\Console\Input\InputInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->mockOutput = $this->getMockBuilder('\Symfony\Component\Console\Output\OutputInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->mockContainer = $this->getMockBuilder('\Symfony\Component\DependencyInjection\ContainerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->mockClickCounterConfig = $this->getMockBuilder('\DotHiv\BusinessBundle\Service\ClickCounterConfig')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->mockBannerRepo = $this->getMockBuilder('\Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+} 
