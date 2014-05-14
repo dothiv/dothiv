a5a2f6d8366422581849295380bcf73c7f443c2a
Markus Tacker <m@coderbyheart.de>
2014-05-14 18:44:44 +0200

    feat(support-us): added support us page

--- src/DotHiv/WebsiteCompanyBundle/Resources/public/app.js
+++ src/DotHiv/WebsiteCompanyBundle/Resources/public/app.js
@@ -56,6 +56,10 @@ angular.module('dotHIVApp', ['dotHIVApp.services', 'dotHIVApp.directives', 'dotH
                 url: '/red-ribbon',
                 templateUrl: '/bundles/dothivwebsitecompany/templates/red-ribbon.html'
             })
+            .state('support-us', {
+                url: '/support-us',
+                templateUrl: '/bundles/dothivwebsitecompany/templates/support-us.html'
+            })
             .state('register', {
                 url: '/register',
                 templateUrl: '/bundles/dothivwebsitecompany/templates/register.html'
