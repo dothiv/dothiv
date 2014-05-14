1c72587eebd5095da39ba707e06d89da8fb83403
Markus Tacker <m@coderbyheart.de>
2014-05-14 18:51:34 +0200

    feat(registry): added registry page

--- src/DotHiv/WebsiteCompanyBundle/Resources/public/app.js
+++ src/DotHiv/WebsiteCompanyBundle/Resources/public/app.js
@@ -60,6 +60,10 @@ angular.module('dotHIVApp', ['dotHIVApp.services', 'dotHIVApp.directives', 'dotH
                 url: '/support-us',
                 templateUrl: '/bundles/dothivwebsitecompany/templates/support-us.html'
             })
+            .state('registry', {
+                url: '/registry',
+                templateUrl: '/bundles/dothivwebsitecompany/templates/registry.html'
+            })
             .state('register', {
                 url: '/register',
                 templateUrl: '/bundles/dothivwebsitecompany/templates/register.html'
