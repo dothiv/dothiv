13184cf250c22b960775540eb405da86a5ef20a9
Markus Tacker <m@coderbyheart.de>
2014-05-26 09:36:49 +0200

    fix(pinkbar): render without angular

--- src/Dothiv/CharityWebsiteBundle/Controller/IndexController.php
+++ src/Dothiv/CharityWebsiteBundle/Controller/IndexController.php
@@ -8,10 +8,26 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends PageController
 {
+    /**
+     * @var float how many EUR to increment on click
+     */
+    private $eurIncrement = 0.1;
+
+    /**
+     * @var float budget available in current stretch; used to calculate status
+     */
+    private $eurGoal = 50000.0;
+
+    /**
+     * @var float
+     */
+    private $alreadyDonated = 100000.0;
+
     public function indexAction(Request $request, $locale)
     {
         // TODO: Cache
-        $data             = $this->buildPageObject($request, $locale, 'index');
+        $data = $this->buildPageObject($request, $locale, 'index');
+        // Projects
         $data['projects'] = $this->getContent()->buildEntries('Project', $locale);
         usort($data['projects'], function (\stdClass $projectA, \stdClass $projectB) {
             if ($projectA->order == $projectB->order) {
@@ -19,8 +35,35 @@ class IndexController extends PageController
             }
             return ($projectA->order < $projectB->order) ? -1 : 1;
         });
+        // Build pinkbar data
+        // TODO: Money format
+        // FIXME: Remove random once live.
+        $already_donated                    = round($this->alreadyDonated * (mt_rand() / mt_getrandmax()), 2);
+        $clicks                             = intval(($this->eurGoal * (1 / $this->eurIncrement)) * (mt_rand() / mt_getrandmax()));
+        $data['pinkbar']                    = array();
+        $data['pinkbar']['donated']         = $already_donated;
+        $data['pinkbar']['donated_label']   = $this->moneyFormat($already_donated, $locale);
+        $unlocked                           = $clicks * $this->eurIncrement;
+        $data['pinkbar']['unlocked']        = $unlocked;
+        $data['pinkbar']['unlocked_label']  = $this->moneyFormat($unlocked, $locale);
+        $data['pinkbar']['percent']         = $unlocked / $this->eurGoal;
+        $data['pinkbar']['clicks']          = $clicks;
+        $data['pinkbar']['increment']       = $this->eurIncrement;
+        $data['pinkbar']['increment_label'] = $this->moneyFormat($this->eurIncrement, $locale);
+        // Build response
         $response = new Response();
         $template = $this->getBundle() . ':Page:index.html.twig';
         return $this->getRenderer()->renderResponse($template, $data, $response);
+    }
+
+    protected function moneyFormat($value, $locale)
+    {
+        switch ($locale) {
+            case 'de':
+                return sprintf('%s €', number_format($value, 2, ',', '.'));
+                break;
+            default:
+                return sprintf('$%s', number_format($value, 2, '.', ','));
+        }
     }
 }
