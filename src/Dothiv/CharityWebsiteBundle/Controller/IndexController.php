5b908955b7181087fc8da54db9d199715eca3c3a
Markus Tacker <m@coderbyheart.de>
2014-05-26 13:21:33 +0200

    feat(index): implement partner listing

--- src/Dothiv/CharityWebsiteBundle/Controller/IndexController.php
+++ src/Dothiv/CharityWebsiteBundle/Controller/IndexController.php
@@ -35,6 +35,11 @@ class IndexController extends PageController
             }
             return ($projectA->order < $projectB->order) ? -1 : 1;
         });
+        // Partners
+        $data['partners'] = $this->getContent()->buildEntries('Partner', $locale);
+        usort($data['partners'], function (\stdClass $partnerA, \stdClass $partnerB) {
+            return strcmp($partnerA->organization, $partnerB->organization);
+        });
         // Build pinkbar data
         // TODO: Money format
         // FIXME: Remove random once live.
