9c6b2952beb2753af36c5d38a44dd6e8977c3275
Markus Tacker <m@coderbyheart.de>
2014-05-27 11:18:46 +0200

    fix(home): hide quotes block on error

--- src/Dothiv/CharityWebsiteBundle/Resources/public/controllers/QuoteController.js
+++ src/Dothiv/CharityWebsiteBundle/Resources/public/controllers/QuoteController.js
@@ -3,12 +3,15 @@
 angular.module('dotHIVApp.controllers').controller('QuoteController', ['$scope', '$http', 'config',
     function ($scope, $http, config) {
 
+        $scope.visible = false;
+
         $scope.quoteOffset = 0;
 
         $scope.quotes = [];
 
         function success(data) {
             $scope.quotes = data;
+            $scope.visible = true;
         }
 
         $http({method: 'GET', url: config.locale + '/content/Quote?markdown=quote'}).success(success);
