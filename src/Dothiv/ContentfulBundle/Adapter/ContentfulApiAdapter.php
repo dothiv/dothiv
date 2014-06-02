bf51e5fe1e2d0d36395a3e235e841334ef9a199a
Markus Tacker <m@coderbyheart.de>
2014-06-02 11:50:13 +0200

    chore(cs): removed unused class uses

--- src/Dothiv/ContentfulBundle/Adapter/ContentfulApiAdapter.php
+++ src/Dothiv/ContentfulBundle/Adapter/ContentfulApiAdapter.php
@@ -2,10 +2,7 @@
 
 namespace Dothiv\ContentfulBundle\Adapter;
 
-use Dothiv\ContentfulBundle\Item\ContentfulItem;
-
 use Psr\Log\LoggerAwareInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 interface ContentfulApiAdapter extends LoggerAwareInterface
 {
