9824e54cd3e86c87eda95ebedc186203dfe0c7a6
Markus Tacker <m@coderbyheart.de>
2014-06-05 15:05:01 +0200

    feat(quotes): show pictures

--- src/Dothiv/CharityWebsiteBundle/Resources/public/controllers/QuoteController.js
+++ src/Dothiv/CharityWebsiteBundle/Resources/public/controllers/QuoteController.js
@@ -9,11 +9,17 @@ angular.module('dotHIVApp.controllers').controller('QuoteController', ['$scope',
 
         $scope.quotes = [];
 
+        var shuffle = function (o) {
+            for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            return o;
+        };
+
         function success(data) {
-            $scope.quotes = data;
+            $scope.quotes = shuffle(data);
             $scope.visible = true;
         }
 
-        $http({method: 'GET', url: '/' + config.locale + '/content/Quote?markdown=quote'}).success(success);
+        $http({method: 'GET', url: '/' + config.locale + '/content/Quote?markdown=quote,description'}).success(success);
+
     }
 ]);
