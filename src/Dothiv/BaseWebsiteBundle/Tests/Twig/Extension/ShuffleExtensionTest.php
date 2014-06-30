e583cc78ba9fc9e638ce2aa74754856ea10a43cf
Markus Tacker <m@coderbyheart.de>
2014-06-30 16:41:31 +0200

    feat(Twig): add shuffle filter

---
+++ src/Dothiv/BaseWebsiteBundle/Tests/Twig/Extension/ShuffleExtensionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Dothiv\BaseWebsiteBundle\Tests\Twig\Extension;
+
+use Dothiv\BaseWebsiteBundle\Twig\Extension\ShuffleTwigExtension;
+
+class ShuffleTwigExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @group BaseWebsiteBundle
+     * @group TwigExtension
+     */
+    public function itShouldBeInstantiable()
+    {
+        $this->assertInstanceOf('\Dothiv\BaseWebsiteBundle\Twig\Extension\ShuffleTwigExtension', $this->getTestObject());
+    }
+
+    /**
+     * @test
+     * @group        BaseWebsiteBundle
+     * @group        TwigExtension
+     * @depends      itShouldBeInstantiable
+     */
+    public function itShouldShuffle()
+    {
+        $array  = array(
+            'Every', 'day', 'I\'m', 'shuffling'
+        );
+        $before = join(' ', $array);
+        $after  = join(' ', $this->getTestObject()->shuffle($array));
+        $this->assertNotEquals($before, $after);
+    }
+
+    /**
+     * @return ShuffleTwigExtension
+     */
+    protected function getTestObject()
+    {
+        return new ShuffleTwigExtension();
+    }
+}
---
+++ src/Dothiv/BaseWebsiteBundle/Twig/Extension/ShuffleTwigExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dothiv\BaseWebsiteBundle\Twig\Extension;
+
+class ShuffleTwigExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('shuffle', array($this, 'shuffle'))
+        );
+    }
+
+    /**
+     * @param array $array
+     *
+     * @return array
+     */
+    public function shuffle($array)
+    {
+        shuffle($array);
+        return $array;
+    }
+
+    public function getName()
+    {
+        return 'dothiv_basewebsite_shuffle';
+    }
+} 
