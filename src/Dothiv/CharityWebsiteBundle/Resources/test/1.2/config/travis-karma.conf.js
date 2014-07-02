207713ec7d3687c6613088b86115a0ad81087e11
Markus Tacker <m@coderbyheart.de>
2014-07-02 18:46:51 +0200

    chore(travis): only enable implemented tests

---
+++ src/Dothiv/CharityWebsiteBundle/Resources/test/1.2/config/travis-karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '../../../../../',
+        frameworks: ['jasmine'],
+        files: [
+            // Angular and other vendor stuff
+            'AngularJsBundle/Resources/angular/angular.js',
+            'AngularJsBundle/Resources/angular/angular-mocks.js',
+            // CharityWebsiteBundle
+            'CharityWebsiteBundle/Resources/public/1.2/app.js',
+            'CharityWebsiteBundle/Resources/public/1.2/filters/*.js',
+            'CharityWebsiteBundle/Resources/public/1.2/controllers/*.js',
+            'CharityWebsiteBundle/Resources/test/1.2/unit/**/*.js',
+            'CharityWebsiteBundle/Resources/test/1.2/controllers/*.js'
+        ],
+        autoWatch: false,
+        singleRun: true,
+        reporters: ['dots', 'junit'],
+        browsers: ['Firefox'],
+        junitReporter: {
+            outputFile: '../../build/logs/karma_unit.xml',
+            suite: 'unit'
+        }
+    });
+};
