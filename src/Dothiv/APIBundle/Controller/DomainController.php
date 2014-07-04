bc31b7b8e3ed3c2bead5ecf93d1c2b5f10084cee
Markus Tacker <m@coderbyheart.de>
2014-07-04 15:32:28 +0200

    chore(domains): fetching of all domains removed

--- src/Dothiv/APIBundle/Controller/DomainController.php
+++ src/Dothiv/APIBundle/Controller/DomainController.php
@@ -88,65 +88,6 @@ class DomainController
     }
 
     /**
-     * Returns one specific domain.
-     *
-     * @ApiDoc(
-     *   section="domain",
-     *   resource=true,
-     *   description="Returns a domain",
-     *   statusCodes={
-     *     200="Returned when successful",
-     *   },
-     *   output="Dothiv\BusinessBundle\Form\DomainType"
-     * )
-     */
-    public function getDomainAction($slug)
-    {
-        // TODO: security concern: who is allowed to GET domain information?
-
-        // retrieve domain from database
-        $domain = $this->getDoctrine()->getManager()->getRepository('DothivBusinessBundle:Domain')->findOneBy(array('id' => $slug));
-        return $this->createForm(new DomainType(), $domain);
-    }
-
-    /**
-     * Returns a list of all domains.
-     *
-     * @QueryParam(name="token", nullable=true, description="Claiming token")
-     *
-     * @ApiDoc(
-     *   section="domain",
-     *   resource=true,
-     *   description="Returns a list of all domains",
-     *   filters={{"name"="token", "dataType"="string"}},
-     *   statusCodes={
-     *     200="Returned when successful",
-     *     400="Token unknown"
-     *   }
-     * )
-     */
-    public function getDomainsAction(ParamFetcher $paramFetcher)
-    {
-        // TODO: security concern: who is allowed to GET domain information?
-
-        // get query parameter and entity manager
-        $token = $paramFetcher->get('token');
-        $em = $this->getDoctrine()->getManager();
-
-        if ($token === null) {
-            // retrieve list of domains from database
-            $list = $em->getRepository('DothivBusinessBundle:Domain')->findAll();
-            return $list;
-        } else {
-            // retrieve requested domain from database
-            $domain = $em->getRepository('DothivBusinessBundle:Domain')->findOneBy(array('claimingToken' => $token));
-            if ($domain === null)
-                throw new HttpException(Codes::HTTP_BAD_REQUEST, 'Invalid token.'); // TODO: better error handling!
-            return $this->createForm(new DomainType(), $domain);
-        }
-    }
-
-    /**
      * Updates the domain.
      *
      * @ApiDoc(
@@ -221,22 +162,5 @@ class DomainController
 
         // return list of banners
         return $domain->getBanners();
-    }
-
-    // ----------- private functions go here -----------
-
-    /**
-     * Generates a 32 digit random code
-     *
-     * Used pool of characters: a-zA-Z0-9
-     */
-    private function newRandomCode()
-    {
-        $pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWWXY0123456789";
-        $code = "";
-        while (strlen($code) < 32) {
-            $code .= substr($pool, rand(0, 61), 1);
-        }
-        return $code;
     }
 }
