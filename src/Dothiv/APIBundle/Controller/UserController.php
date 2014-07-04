504a9967f8129ff31edfde7951b3bc8527fb6ccf
Markus Tacker <m@coderbyheart.de>
2014-07-04 15:33:00 +0200

    chore(user): removed login and user edit

--- src/Dothiv/APIBundle/Controller/UserController.php
+++ src/Dothiv/APIBundle/Controller/UserController.php
@@ -3,11 +3,9 @@
 namespace Dothiv\APIBundle\Controller;
 
 use Dothiv\BusinessBundle\Entity\User;
-use Dothiv\BusinessBundle\Form\UserEditType;
 use Dothiv\BusinessBundle\Repository\DomainRepositoryInterface;
 use JMS\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Security\Core\SecurityContext;
 
 class UserController
@@ -23,7 +21,6 @@ class UserController
     private $securityContext;
 
     public function __construct(
-
         SecurityContext $securityContext,
         DomainRepositoryInterface $domainRepo,
         Serializer $serializer
@@ -32,73 +29,6 @@ class UserController
         $this->domainRepo      = $domainRepo;
         $this->securityContext = $securityContext;
         $this->serializer      = $serializer;
-    }
-
-    /**
-     * Returns the requested user.
-     *
-     * @ApiDoc(
-     *   section="user",
-     *   resource=true,
-     *   description="Gets information about the requested user",
-     *   statusCodes={
-     *     200="Successful",
-     *     403="Access denied"
-     *   },
-     *   output="Dothiv\BusinessBundle\Entity\User"
-     * )
-     *
-     * @Secure(roles="ROLE_USER")
-     */
-    public function getUserAction($slug)
-    {
-        $context = $this->get('security.context');
-        if ($context->isGranted('ROLE_ADMIN') || $context->getToken()->getUsername() == $slug) {
-            $user = $this->getDoctrine()->getManager()->getRepository('DothivBusinessBundle:User')->findOneBy(array('username' => $slug));
-            return $this->createForm(new UserEditType(), $user);
-        }
-        throw new HttpException(403);
-    }
-
-    /**
-     * Updates the requested user.
-     *
-     * @ApiDoc(
-     *   section="user",
-     *   resource=true,
-     *   description="Updates the requested user.",
-     *   statusCodes={
-     *     200="Successful",
-     *     403="Access denied"
-     *   },
-     *   output="Dothiv\BusinessBundle\Entity\User"
-     * )
-     *
-     * @Secure(roles="ROLE_USER")
-     */
-    public function putUserAction($slug)
-    {
-        $context = $this->get('security.context');
-        if ($context->isGranted('ROLE_ADMIN') || $context->getToken()->getUsername() == $slug) {
-            // fetch user from database
-            $em   = $this->getDoctrine()->getManager();
-            $user = $em->getRepository('DothivBusinessBundle:User')->findOneBy(array('username' => $slug));
-
-            // apply form
-            $form = $this->createForm(new UserEditType(), $user);
-            $form->bind($this->getRequest());
-
-            if ($form->isValid()) {
-                $em->persist($user);
-                $this->container->get('fos_user.user_manager')->updateUser($user, false);
-                $em->flush();
-
-                return null;
-            }
-
-            return array('form' => $form);
-        }
-        throw new HttpException(403);
     }
 
     /**
