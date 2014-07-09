62ba2af8fd48deb222e0579cd5a43fd1956c1b1e
Markus Tacker <m@coderbyheart.de>
2014-07-09 09:09:29 +0200

    chore(tests): fix angular tests

--- src/Dothiv/CharityWebsiteBundle/Resources/test/1.2/config/travis-karma.conf.js
+++ src/Dothiv/CharityWebsiteBundle/Resources/test/1.2/config/travis-karma.conf.js
@@ -5,11 +5,16 @@ module.exports = function (config) {
         files: [
             // Angular and other vendor stuff
             'AngularJsBundle/Resources/angular/angular.js',
+            'AngularJsBundle/Resources/angular/angular-resource.js',
+            'AngularJsBundle/Resources/angular/angular-route.js',
+            'AngularJsBundle/Resources/extra/angular-ui.github.io/ui-router/angular-ui-router.js',
             'AngularJsBundle/Resources/angular/angular-mocks.js',
             // CharityWebsiteBundle
             'CharityWebsiteBundle/Resources/public/1.2/app.js',
             'CharityWebsiteBundle/Resources/public/1.2/filters/*.js',
             'CharityWebsiteBundle/Resources/public/1.2/controllers/*.js',
+            'CharityWebsiteBundle/Resources/public/1.2/services/*.js',
+            'CharityWebsiteBundle/Resources/public/1.2/services/models/*.js',
             'CharityWebsiteBundle/Resources/test/1.2/unit/**/*.js',
             'CharityWebsiteBundle/Resources/test/1.2/controllers/*.js'
         ],
