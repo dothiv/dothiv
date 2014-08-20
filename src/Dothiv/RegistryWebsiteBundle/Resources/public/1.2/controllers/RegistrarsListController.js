6975f27b134fc3b4ba7793d2a3c1df433c2086ee
Markus Tacker <m@coderbyheart.de>
2014-08-20 18:48:28 +0200

    feat(registrars): made registrar list sortable
    See https://trello.com/c/ZdULbBwJ

--- src/Dothiv/RegistryWebsiteBundle/Resources/public/1.2/controllers/RegistrarsListController.js
+++ src/Dothiv/RegistryWebsiteBundle/Resources/public/1.2/controllers/RegistrarsListController.js
@@ -1,16 +1,49 @@
 'use strict';
 
-angular.module('dotHIVApp.controllers').controller('RegistrarsListController', ['$scope', '$http', 'config', function ($scope, $http, config) {
-    $scope.registrars = [];
+angular.module('dotHIVApp.controllers').controller('RegistrarsListController', ['$scope', '$http', 'config', 'MoneyFormatter', '$sce',
+    function ($scope, $http, config, MoneyFormatter, $sce) {
 
-    var shuffle = function (o) {
-        for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-        return o;
-    };
+        $scope.predicate = 'random';
 
-    function success(data) {
-        $scope.registrars = shuffle(data);
-    }
+        var mf = new MoneyFormatter(config.locale);
+        $scope.registrars = [];
 
-    $http({method: 'GET', url: '/' + config.locale + '/content/Registrar'}).success(success);
-}]);
+        function buildPrice(registrar) {
+            if (typeof  registrar.pricePerYear == 'undefined') {
+                return ["–", Infinity];
+            }
+            var value = parseInt(registrar.pricePerYear, 10);
+            var sortValue = value;
+            if (registrar.pricePerYearCurrency == 'USD') {
+                sortValue = sortValue / config.eur_to_usd;
+                var valueLabel = mf.decimalFormat(value, '$');
+            } else {
+                var valueLabel = mf.decimalFormat(value, '€');
+            }
+            return [valueLabel, sortValue]
+        }
+
+        function success(data) {
+            var registrars = [];
+            var num = data.length;
+            for (var k in data) {
+                var price = buildPrice(data[k]);
+                registrars.push(
+                    {
+                        name: data[k].name,
+                        image: data[k].image,
+                        country: data[k].country,
+                        priceLabel: price[0],
+                        price: price[1],
+                        url: data[k].url,
+                        promotion: $sce.trustAsHtml(data[k].promotion),
+                        random: parseInt(Math.random() * num)
+                    }
+                );
+
+            }
+            $scope.registrars = registrars;
+        }
+
+        $http({method: 'GET', url: '/' + config.locale + '/content/Registrar?markdown=promotion:inline'}).success(success);
+    }]);
