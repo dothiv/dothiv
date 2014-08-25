9139e347cc2032417b044693edca2cddb0648f4b
Markus Tacker <m@coderbyheart.de>
2014-08-25 18:16:08 +0200

    feat(premium-configurator): lawyer up checkout

--- src/Dothiv/PremiumConfiguratorBundle/Resources/public/js/controllers/SubscriptionController.js
+++ src/Dothiv/PremiumConfiguratorBundle/Resources/public/js/controllers/SubscriptionController.js
@@ -1,19 +1,36 @@
 'use strict';
 
-angular.module('dotHIVApp.controllers').controller('SubscriptionController', ['$rootScope', '$scope', 'security', 'config', 'dothivPremiumSubscription',
-    function ($rootScope, $scope, security, config, dothivPremiumSubscription) {
+angular.module('dotHIVApp.controllers').controller('SubscriptionController', ['$rootScope', '$scope', 'security', 'config', 'dothivPremiumSubscription', '$http',
+    function ($rootScope, $scope, security, config, dothivPremiumSubscription, $http) {
         $scope.block = 'pc.subscription.checking';
         $scope.domain = config.domain;
+        $scope.subscription = {
+            type: null,
+            name: null,
+            address1: null,
+            address2: null,
+            taxNo: null,
+            vatNo: null,
+            cycle_start: config.cycle_start,
+            cycle_end: config.cycle_end,
+            vat: config.vat.de
+        };
+        $scope.countries = $http.get('/bundles/dothivbasewebsite/data/countries.json');
 
-        $scope.subscription = dothivPremiumSubscription.get(
-            {domain: config.domain},
-            function () {
-                $scope.block = 'pc.subscription.subscribed';
-            },
-            function () {
-                $scope.block = 'pc.subscription.nosubscription';
-            }
-        );
+
+        $scope.block = 'pc.subscription.nosubscription';
+
+        /*
+         $scope.subscription = dothivPremiumSubscription.get(
+         {domain: config.domain},
+         function () {
+         $scope.block = 'pc.subscription.subscribed';
+         },
+         function () {
+         $scope.block = 'pc.subscription.nosubscription';
+         }
+         );
+         */
 
         var handler = StripeCheckout.configure({
             key: config.stripe.publishableKey,
@@ -41,6 +58,51 @@ angular.module('dotHIVApp.controllers').controller('SubscriptionController', ['$
                 email: security.user.email,
                 panelLabel: config.strings.pc.stripe.checkout.button
             });
+        };
+
+        // Form stuff
+        $scope.subscriptionStep = 'form';
+        $scope.resetSubscriptionForm = function () {
+            $scope.subscription.type = null;
+        };
+
+        $scope.vatNoRequired = function () {
+            console.log($scope.subscription);
+            var vatNoRequired = $scope.subscription.type == 'euorgnet'
+                || $scope.subscription.type == 'deorg';
+            if (vatNoRequired) {
+                if ($scope.subscription.type == 'deorg') {
+                    if ($scope.subscription.taxNo) {
+                        return false;
+                    }
+                }
+            }
+            return vatNoRequired;
+        };
+
+        $scope.taxNoRequired = function () {
+            var taxNoRequired = $scope.subscription.type == 'deorg';
+            if (taxNoRequired && $scope.subscription.vatNo) {
+                return false;
+            }
+            return taxNoRequired;
+        };
+
+        $scope.vatTaxNoRequired = function () {
+            return $scope.vatNoRequired() || $scope.taxNoRequired();
         }
+
+        $scope.totalIncludesTax = function () {
+            return $scope.subscription.type == 'euorgnet'
+                || $scope.subscription.type == 'euorg'
+                || $scope.subscription.type == 'deorg'
+                || $scope.subscription.type == 'euprivate';
+        }
+
+        $scope.$watch('$scope.subscription.type', function (type) {
+            if ($scope.subscription.type == 'deorg') {
+                $scope.subscription.country = 'Deutschland';
+            }
+        });
     }
 ]);
