642e059e83cb3f80e12083f3faed792699f633a8
root <root@nanshe.tacker.lan>
2014-08-26 15:00:55 +0200

    feat(qlp): show iframe with target site

--- src/Dothiv/QLPPartnerBundle/Resources/public/js/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('dotHIVApp', ['dotHIVApp.services', 'dotHIVApp.controllers']);
-angular.module('dotHIVApp.services', ['dotHIVApp.controllers']);
-angular.module('dotHIVApp.controllers', []);
-
