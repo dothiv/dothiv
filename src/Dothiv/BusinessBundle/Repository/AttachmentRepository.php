563f5eeaf93305230e7e07d9f6159b03cf88b284
Markus Tacker <m@coderbyheart.de>
2014-09-30 15:26:17 +0200

    feat(doctrine): validate all teh things!
    See https://trello.com/c/PVJJwG6U

--- src/Dothiv/BusinessBundle/Repository/AttachmentRepository.php
+++ src/Dothiv/BusinessBundle/Repository/AttachmentRepository.php
@@ -4,16 +4,19 @@ namespace Dothiv\BusinessBundle\Repository;
 
 use Dothiv\BusinessBundle\Entity\Attachment;
 use Doctrine\ORM\EntityRepository as DoctrineEntityRepository;
+use Dothiv\BusinessBundle\Repository\Traits\ValidatorTrait;
 use PhpOption\Option;
 
 class AttachmentRepository extends DoctrineEntityRepository implements AttachmentRepositoryInterface
 {
+    use ValidatorTrait;
+    
     /**
      * {@inheritdoc}
      */
     public function persist(Attachment $attachment)
     {
-        $this->getEntityManager()->persist($attachment);
+        $this->getEntityManager()->persist($this->validate($attachment));
         return $this;
     }
 
