9294266ba1a6e607ef3df173a27a67691e665e32
Markus Tacker <m@coderbyheart.de>
2014-10-02 14:13:33 +0200

    chore(unused): clean up

--- src/Dothiv/BaseWebsiteBundle/Resources/test/test.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('dotHIVApp.services', ['ui.bootstrap', 'ui.state', 'ngResource', 'http-auth-interceptor']);
-angular.module('dotHIVApp.directives', []);
-angular.module('dotHIVApp.filters', []);
