f9dbb34ea0861b1cc536dfa0bbc4c1eb87046737
Markus Tacker <m@coderbyheart.de>
2014-10-17 12:47:41 +0200

    fix(Click-Counter): fix subcommand call

--- src/Dothiv/CharityWebsiteBundle/Command/SendClickCounterConfigurationToUnconfiguredCommand.php
+++ src/Dothiv/CharityWebsiteBundle/Command/SendClickCounterConfigurationToUnconfiguredCommand.php
@@ -3,6 +3,7 @@
 namespace Dothiv\CharityWebsiteBundle\Command;
 
 use Dothiv\CharityWebsiteBundle\Service\SendClickCounterConfigurationServiceInterface;
+use Dothiv\ValueObject\HivDomainValue;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
@@ -25,10 +26,21 @@ class SendClickCounterConfigurationToUnconfiguredCommand extends ContainerAwareC
         /** @var SendClickCounterConfigurationServiceInterface $service */
         $service = $this->getContainer()->get('dothiv.charity.clickcounter_notification');
         foreach ($service->findDomainsToBeNotified() as $domain) {
+            if ($output->getVerbosity() > OutputInterface::VERBOSITY_QUIET) {
+                $output->writeln(
+                    sprintf(
+                        'Notifying %s for %s',
+                        $domain->getOwnerEmail(),
+                        HivDomainValue::create($domain->getName())->toUTF8()
+                    )
+                );
+            }
             $command = $this->getApplication()->find('charity:clickcounter:send-configuration');
             $input   = new ArrayInput(array(
-                'domain' => $domain->getName()
+                'command' => 'charity:clickcounter:send-configuration',
+                'domain'  => $domain->getName()
             ));
+
             $command->run($input, $output);
         }
     }
