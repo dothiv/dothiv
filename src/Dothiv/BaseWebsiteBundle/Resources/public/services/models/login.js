ff2e084d351e90342206934443d469443838472b
Markus Tacker <m@coderbyheart.de>
2014-10-23 09:11:00 +0200

    chore(dead code): removed unused js code

--- src/Dothiv/BaseWebsiteBundle/Resources/public/services/models/login.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('dotHIVApp.services').factory('dothivLoginResource', function($resource) {
-    return $resource('api/login', {}, {
-        'get':    {method:'GET'},
-        'login':  {method:'POST'},
-        'logout': {method:'DELETE'}
-    });
-});
