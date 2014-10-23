05c424d59c71915828a7d91d7e86707163137f96
Markus Tacker <m@coderbyheart.de>
2014-10-23 09:11:00 +0200

    chore(dead code): removed unused js code

--- src/Dothiv/BaseWebsiteBundle/Resources/public/services/models/locale.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('dotHIVApp.services').factory('dothivLocaleResource', function($resource) {
-    return $resource('api/locale', {}, {
-        'get':    {method:'GET'},
-        'put':    {method:'PUT'},
-    });
-});
--- src/Dothiv/BaseWebsiteBundle/Resources/test/1.2/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('dotHIVApp', ['dotHIVApp.services', 'dotHIVApp.filters', 'dotHIVApp.controllers']);
-angular.module('dotHIVApp.services', ['dotHIVApp.controllers']);
-angular.module('dotHIVApp.controllers', []);
-angular.module('dotHIVApp.filters', []);
