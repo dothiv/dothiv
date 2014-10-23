f4282d055fc7a97323c276c92f312940a9d14bd4
Markus Tacker <m@coderbyheart.de>
2014-10-23 09:14:11 +0200

    refactor(JS tests): reorganized JS tests

---
+++ app/Resources/test/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('dotHIVApp', ['dotHIVApp.services', 'dotHIVApp.filters', 'dotHIVApp.controllers']);
+angular.module('dotHIVApp.services', ['dotHIVApp.controllers']);
+angular.module('dotHIVApp.controllers', []);
+angular.module('dotHIVApp.filters', []);
