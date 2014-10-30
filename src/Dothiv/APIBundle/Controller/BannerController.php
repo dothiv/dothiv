d5607367b5da2fadf6d490f512e772bce46d1a3c
Markus Tacker <m@coderbyheart.de>
2014-10-30 15:31:59 +0100

    chore(dead code): removed

--- src/Dothiv/APIBundle/Controller/BannerController.php
+++ src/Dothiv/APIBundle/Controller/BannerController.php
@@ -64,68 +64,6 @@ class BannerController
     }
 
     /**
-     * Returns a specific banner.
-     *
-     * @ApiDoc(
-     *   section="banner",
-     *   resource=true,
-     *   description="Returns a banner",
-     *   statusCodes={
-     *     200="Returned when successful",
-     *   },
-     *   output="Dothiv\BusinessBundle\Form\BannerType"
-     * )
-     */
-    public function getBannerAction($id)
-    {
-        // TODO: security concern: who is allowed to GET banner information?
-
-        // retrieve banner from database
-        $banner = $this->getDoctrine()->getManager()->getRepository('DothivBusinessBundle:Banner')->findOneBy(array('id' => $id));
-        return $this->createForm(new BannerType(), $banner);
-    }
-
-    /**
-     * Creates a new banner.
-     *
-     * @ApiDoc(
-     *   section="banner",
-     *   resource=true,
-     *   description="Creates a new banner",
-     *   statusCodes={
-     *     201="Successfully created"
-     *   },
-     *   output="Dothiv\BusinessBundle\Form\BannerType"
-     * )
-     */
-    public function postBannersAction()
-    {
-        // TODO: security concern: who is allowed to create new banners?
-        $banner = new Banner();
-
-        $form = $this->createForm(new BannerType(), $banner);
-        $form->bind($this->getRequest());
-
-        if ($form->isValid()) {
-            // push configuration
-            if ($banner->getDomain())
-                $this->get('clickcounter')->setup($banner->getDomain(), $banner);
-
-            // persist the new banner
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($banner);
-            $em->flush();
-
-            // prepare response
-            $response = $this->redirectView($this->generateUrl('get_banner', array('id' => $banner->getId())), Codes::HTTP_CREATED);
-            $response->setData($this->createForm(new BannerType(), $banner));
-            return $response;
-        }
-
-        return array('form' => $form);
-    }
-
-    /**
      * Updates the banner for the given domain.
      *
      * @ApiRequest("Dothiv\APIBundle\Request\BannerConfigRequest")
@@ -183,30 +121,5 @@ class BannerController
         $response = $this->createResponse();
         $response->setContent($this->serializer->serialize($banner, 'json'));
         return $response;
-    }
-
-    /**
-     * Gets the banners of this domain.
-     *
-     * @ApiDoc(
-     *   section="domain",
-     *   resource=true,
-     *   description="Gets a list of banners of this domain",
-     *   statusCodes={
-     *     200="Successful",
-     *     403="Access denied"
-     *   },
-     *   output="Dothiv\BusinessBundle\Entity\Banner"
-     * )
-     */
-    public function getDomainBannersAction($id)
-    {
-        // TODO: security concern: who is allowed to get domain banners?
-
-        // retrieve domain from database
-        $domain = $this->getDoctrine()->getManager()->getRepository('DothivBusinessBundle:Domain')->findOneBy(array('id' => $id));
-
-        // return list of banners
-        return $domain->getBanners();
     }
 }
