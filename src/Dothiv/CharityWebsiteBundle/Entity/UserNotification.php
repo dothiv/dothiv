5b28c9e4c6bd5e6193cc7b9ada612dc46a92757b
Markus Tacker <m@coderbyheart.de>
2014-11-03 17:11:28 +0100

    feat(email-address-change): add notification

---
+++ src/Dothiv/CharityWebsiteBundle/Entity/UserNotification.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Dothiv\CharityWebsiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Tests\ORM\Tools\Export\User;
+use Dothiv\BusinessBundle\Entity\EntityInterface;
+use Dothiv\BusinessBundle\Entity\Traits;
+use JMS\Serializer\Annotation as Serializer;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Represents a dismissable notification for a user
+ *
+ * @ORM\Entity(repositoryClass="Dothiv\CharityWebsiteBundle\Repository\NotificationRepository")
+ * @ORM\Table(
+ *  name="CharityUserNotification",
+ *  indexes={
+ *      @ORM\Index(name="charity_user_notification__user_idx", columns={"user_id"})
+ *  }
+ * )
+ * @Serializer\ExclusionPolicy("all")
+ */
+class UserNotification implements EntityInterface
+{
+    use Traits\CreateUpdateTime;
+
+    /**
+     * database primary key
+     *
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Dothiv\BusinessBundle\Entity\User")
+     * @ORM\JoinColumn(onDelete="CASCADE")
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * notification properties
+     *
+     * @var array
+     * @ORM\Column(type="json_array", nullable=false)
+     * @Assert\Type("array")
+     */
+    protected $properties = array();
+
+    /**
+     * Notification has been dismissed by the user.
+     *
+     * @var bool
+     * @ORM\Column(type="boolean", nullable=false)
+     * @Assert\Type("boolean")
+     */
+    protected $dismissed = false;
+
+    /**
+     * @return boolean
+     */
+    public function getDismissed()
+    {
+        return $this->dismissed;
+    }
+
+    /**
+     * @param boolean $dismissed
+     *
+     * @return self
+     */
+    public function setDismissed($dismissed)
+    {
+        $this->dismissed = (bool)$dismissed;
+        return $this;
+    }
+
+    /**
+     * Dismiss the notification.
+     */
+    public function dismiss()
+    {
+        $this->setDismissed(true);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPublicId()
+    {
+        return $this->getId();
+    }
+
+    /**
+     * @return array
+     */
+    public function getProperties()
+    {
+        return $this->properties;
+    }
+
+    /**
+     * @param array $properties
+     *
+     * @return self
+     */
+    public function setProperties(array $properties)
+    {
+        $this->properties = $properties;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return self
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+}
