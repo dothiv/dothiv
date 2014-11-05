eeadca55a62599afa0cc86dab0cb30793314daa0
Markus Tacker <m@coderbyheart.de>
2014-11-05 15:25:47 +0100

    refactor(exceptions): always use ExceptionInterface in Exception ns

--- src/Dothiv/AfiliasImporterBundle/Exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Dothiv\AfiliasImporterBundle;
-
-interface Exception
-{
-
-} 
--- src/Dothiv/PremiumConfiguratorBundle/Exception/InvalidArgumentException.php
+++ src/Dothiv/PremiumConfiguratorBundle/Exception/InvalidArgumentException.php
@@ -2,6 +2,6 @@
 
 namespace Dothiv\PremiumConfiguratorBundle\Exception;
 
-class InvalidArgumentException extends \InvalidArgumentException implements Exception
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
 {
 }
