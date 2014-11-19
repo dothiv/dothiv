33b38993888b79371e7c46d948404319ab42d9b0
Markus Tacker <m@coderbyheart.de>
2014-11-19 11:13:21 +0100

    feat(charity): redesign startpage: mobile navigation

--- src/Dothiv/CharityWebsiteBundle/Resources/public/js/controllers/HeaderController.js
+++ src/Dothiv/CharityWebsiteBundle/Resources/public/js/controllers/HeaderController.js
@@ -5,12 +5,21 @@ angular.module('dotHIVApp.controllers').controller('HeaderController', ['$scope'
         $scope.loggedIn = false;
         $scope.isAuthenticated = function () {
             return $scope.loggedIn;
-        }
+        };
 
         $scope.login = function () {
-        }
+        };
 
         $scope.logout = function () {
-        }
+        };
+
+        // Menu stuff
+        $scope.expanded = false;
+        $scope.toggle = function () {
+            $scope.expanded = !$scope.expanded;
+        };
+        $scope.close = function () {
+            $scope.expanded = false;
+        };
     }
 ]);
