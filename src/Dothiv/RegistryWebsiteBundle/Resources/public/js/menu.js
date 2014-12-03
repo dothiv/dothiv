9383040d3a5f09702caed7178f34705250e12358
Markus Tacker <m@coderbyheart.de>
2014-12-03 12:04:28 +0100

    fix(registry): mobile menu fix for Firefox 34
    FF34 repost the screen size to be 960px via .width()
    using window.matchMedia fixes the problem
    
    See https://trello.com/c/3FLPvVol

--- src/Dothiv/RegistryWebsiteBundle/Resources/public/js/menu.js
+++ src/Dothiv/RegistryWebsiteBundle/Resources/public/js/menu.js
@@ -1,19 +1,19 @@
 'use strict';
 
 $(document).ready(function () {
-    if ($(window).width() < 768) {
+    if ((typeof window.matchMedia != 'undefined' && window.matchMedia("(max-width: 768px)").matches) || $(window).width() < 768) {
         $("#mainmenu").mmenu({
             classes: "mm-light",
             offCanvas: {
                 zposition: "front"
             }
         })
-        .on("opening.mm", function () {
-            $("#mainmenu-overlay").removeClass("hidden");
-        })
-        .on("closing.mm", function () {
-            $("#mainmenu-overlay").addClass("hidden");
-        });
+            .on("opening.mm", function () {
+                $("#mainmenu-overlay").removeClass("hidden");
+            })
+            .on("closing.mm", function () {
+                $("#mainmenu-overlay").addClass("hidden");
+            });
         $('div.mm-page').after('<div id="mainmenu-overlay" class="hidden"></div>');
     }
 });
