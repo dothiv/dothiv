100e2c673a0b5666f13dde15c800f2c3e51fbb9c
Markus Tacker <m@coderbyheart.de>
2014-12-09 18:29:40 +0100

    feat(hiv-domain-status): begin domain listener
    This listener sets a flag on the Domain entity for the .hiv domain status.

---
+++ src/Dothiv/BusinessBundle/Listener/HivDomainStatusDomainCheckListener.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Dothiv\BusinessBundle\Listener;
+
+/**
+ * This listener sets a flag on the Domain entity for the .hiv domain status.
+ *
+ * FIXME: Implement
+ */
+class HivDomainStatusDomainCheckListener
+{
+
+}
