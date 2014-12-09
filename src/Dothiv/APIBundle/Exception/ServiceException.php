1193386b59a40e4c8764c9082f2df375b81d2cfb
Markus Tacker <m@coderbyheart.de>
2014-12-09 14:33:45 +0100

    feat(json-ld): extract api client

---
+++ src/Dothiv/APIBundle/Exception/ServiceException.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace Dothiv\APIBundle\Exception;
+
+class ServiceException extends \RuntimeException implements ExceptionInterface
+{
+
+}
--- src/Dothiv/AfiliasImporterBundle/Service/AfiliasImporterService.php
+++ src/Dothiv/AfiliasImporterBundle/Service/AfiliasImporterService.php
@@ -7,6 +7,7 @@ use Dothiv\AfiliasImporterBundle\AfiliasImporterBundleEvents;
 use Dothiv\AfiliasImporterBundle\Event\DomainTransactionEvent;
 use Dothiv\AfiliasImporterBundle\Exception\ServiceException;
 use Dothiv\AfiliasImporterBundle\Model\PaginatedList;
+use Dothiv\APIBundle\JsonLd\Client\JsonLdGuzzleClient;
 use Dothiv\ValueObject\URLValue;
 use Guzzle\Http\ClientInterface;
 use Guzzle\Http\Message\Response;
@@ -16,7 +17,7 @@ class AfiliasImporterService implements AfiliasImporterServiceInterface
 {
 
     /**
-     * @var \Guzzle\Http\ClientInterface
+     * @var JsonLdGuzzleClient
      */
     private $client;
 
@@ -31,7 +32,7 @@ class AfiliasImporterService implements AfiliasImporterServiceInterface
      */
     public function __construct(ClientInterface $client, EventDispatcherInterface $eventDispatcher)
     {
-        $this->client          = $client;
+        $this->client          = new JsonLdGuzzleClient($client);
         $this->eventDispatcher = $eventDispatcher;
     }
 
@@ -40,15 +41,13 @@ class AfiliasImporterService implements AfiliasImporterServiceInterface
      */
     public function fetchRegistrations(URLValue $url)
     {
-        $request  = $this->client->get((string)$url, array('Accept' => 'application/json'));
-        $response = $request->send();
-        $list     = $this->getListResponse($response, '\Dothiv\AfiliasImporterBundle\Event\DomainRegisteredEvent');
+        $list = $this->client->getListResponse($url, '\Dothiv\AfiliasImporterBundle\Event\DomainRegisteredEvent');
 
         foreach ($list->getItems() as $event) {
             $this->eventDispatcher->dispatch(AfiliasImporterBundleEvents::DOMAIN_REGISTERED, $event);
         }
 
-        return $list->getNextUrl();
+        return $list->getNextPageUrl();
     }
 
     /**
@@ -56,9 +55,7 @@ class AfiliasImporterService implements AfiliasImporterServiceInterface
      */
     public function fetchTransactions(URLValue $url)
     {
-        $request  = $this->client->get((string)$url, array('Accept' => 'application/json'));
-        $response = $request->send();
-        $list     = $this->getListResponse($response, '\Dothiv\AfiliasImporterBundle\Event\DomainTransactionEvent');
+        $list = $this->client->getListResponse($url, '\Dothiv\AfiliasImporterBundle\Event\DomainTransactionEvent');
 
         foreach ($list->getItems() as $event) {
             /** @var DomainTransactionEvent $event */
@@ -103,88 +100,6 @@ class AfiliasImporterService implements AfiliasImporterServiceInterface
             $this->eventDispatcher->dispatch($type, $event);
         }
 
-        return $list->getNextUrl();
-    }
-
-    protected function getListResponse(Response $response, $itemClass)
-    {
-        if ($response->getContentType() != 'application/json') {
-            throw new ServiceException(
-                sprintf(
-                    'Unexpected contenttype "%s", expected "application/json"', $response->getContentType()
-                )
-            );
-        }
-        $data = json_decode($response->getBody());
-        if (!is_object($data)) {
-            throw new ServiceException(
-                sprintf(
-                    'Unexpected %s, expected object', gettype($data)
-                )
-            );
-        }
-
-        $this->expectProperty($data, '@context', 'http://jsonld.click4life.hiv/List');
-        $context = $id = constant("$itemClass::CONTEXT");
-        $this->expectProperty($data, '@type', $context);
-        $list = new PaginatedList();
-        $list->setTotal($data->total);
-        foreach ($data->items as $item) {
-            $instance = new $itemClass();
-            foreach (get_class_vars($itemClass) as $k => $v) {
-                $instance->$k = $item->$k;
-
-            }
-            $list->add($instance);
-        }
-
-        $list->setNextUrl($this->getNextUrl($response));
-        return $list;
-    }
-
-    /**
-     * @param \stdClass $object
-     * @param string    $property
-     * @param string    $expectedValue
-     *
-     * @return mixed
-     * @throws \Dothiv\AfiliasImporterBundle\Exception\ServiceException
-     */
-    protected function expectProperty(\stdClass $object, $property, $expectedValue)
-    {
-        if (!property_exists($object, $property)) {
-            throw new ServiceException(
-                sprintf(
-                    'Missing %s in "%s"', $property, substr(json_encode($object), 0, 25)
-                )
-            );
-        }
-        $value = $object->{$property};
-        if ($value != $expectedValue) {
-            throw new ServiceException(
-                sprintf(
-                    'Invalid %s "%s", expected "%s" in "%s"', $property, $value, $expectedValue, substr(json_encode($object), 0, 25)
-                )
-            );
-        }
-        return $value;
-    }
-
-    /**
-     * @param Response $response
-     *
-     * @return URLValue|null
-     */
-    protected function getNextUrl(Response $response)
-    {
-        $linkHeader = $response->getHeader('Link');
-        if (!$linkHeader) {
-            return null;
-        }
-        if (preg_match('/<([^>]+)>; *rel="next"/', $linkHeader, $nextMatch)) {
-            $path  = $nextMatch[1];
-            $parts = parse_url($response->getEffectiveUrl());
-            return new URLValue(sprintf('%s://%s:%d%s', $parts['scheme'], $parts['host'], $parts['port'], $path));
-        }
+        return $list->getNextPageUrl();
     }
 }
