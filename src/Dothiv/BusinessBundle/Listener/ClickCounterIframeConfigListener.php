9f65033864ca98c5fd6504a4c65ea0163d4f88c0
Markus Tacker <m@coderbyheart.de>
2014-12-11 16:41:12 +0100

    feat(clickcounter-iframe): add command for updating the iframe config

---
+++ src/Dothiv/BusinessBundle/Listener/ClickCounterIframeConfigListener.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Dothiv\BusinessBundle\Listener;
+
+use Dothiv\BusinessBundle\Event\ClickCounterConfigurationEvent;
+use Dothiv\ValueObject\URLValue;
+use Guzzle\Http\Client;
+
+/**
+ * This listener updates the iframe configuration on the CNAME instance.
+ */
+class ClickCounterIframeConfigListener
+{
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var URLValue
+     */
+    private $serviceUrl;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @param Client $client
+     * @param string $password
+     * @param string $serviceUrl
+     * @param string $username
+     */
+    public function __construct(Client $client, $serviceUrl, $username, $password)
+    {
+        $this->client     = $client;
+        $this->serviceUrl = new URLValue($serviceUrl);
+        $this->username   = $username;
+        $this->password   = $password;
+    }
+
+    /**
+     * @param ClickCounterConfigurationEvent $event
+     */
+    public function onClickCounterConfiguration(ClickCounterConfigurationEvent $event)
+    {
+        $domain = $event->getDomain();
+        $config = $event->getConfig();
+
+        if (!isset($config['redirect_url'])) {
+            $this->client->delete(
+                $this->serviceUrl->toScalar() . 'domain/' . $domain->getName(),
+                null,
+                null,
+                array(
+                    'auth' => array($this->username, $this->password)
+                )
+            )->send();
+        } else {
+            $this->client->put(
+                $this->serviceUrl->toScalar() . 'domain/' . $domain->getName(),
+                array('Content-Type' => 'application/json'),
+                json_encode(array('redirect' => $config['redirect_url']), JSON_UNESCAPED_SLASHES),
+                array(
+                    'auth' => array($this->username, $this->password)
+                )
+            )->send();
+        }
+    }
+}
