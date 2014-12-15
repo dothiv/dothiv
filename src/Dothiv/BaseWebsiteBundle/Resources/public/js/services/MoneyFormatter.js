0c6a7ad72a860c8518996bd6a11347b01b4ba70d
Markus Tacker <m@coderbyheart.de>
2014-12-15 18:35:06 +0100

    feat(basewebsite): add float formatting for money

--- src/Dothiv/BaseWebsiteBundle/Resources/public/js/services/MoneyFormatter.js
+++ src/Dothiv/BaseWebsiteBundle/Resources/public/js/services/MoneyFormatter.js
@@ -11,12 +11,11 @@ angular.module('dotHIVApp.services').factory('MoneyFormatter', [function () {
 
     /**
      * @see http://stackoverflow.com/a/149099
-     * @param c
-     * @param d
-     * @param t
+     * @param c the number to format
+     * @param t the thousands separator
      * @returns {string}
      */
-    MoneyFormatter.prototype.formatMoney = function (c, d, t) {
+    MoneyFormatter.prototype.formatDecimalNumber = function (c, t) {
         var i = parseInt(c, 10) + "";
         var j = (j = i.length) > 3 ? j % 3 : 0;
         return (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t);
@@ -25,9 +24,20 @@ angular.module('dotHIVApp.services').factory('MoneyFormatter', [function () {
     MoneyFormatter.prototype.decimalFormat = function (value, currencySymbol) {
         switch (this.locale) {
             case 'de':
-                return this.formatMoney(value, '', '.') + ' ' + currencySymbol;
+                return this.formatDecimalNumber(value, '.') + ' ' + currencySymbol;
             default:
-                return currencySymbol + this.formatMoney(value, '', ',');
+                return currencySymbol + this.formatDecimalNumber(value, ',');
+        }
+    };
+
+    MoneyFormatter.prototype.format = function (value, currencySymbol) {
+        var f = Math.round((value - Math.floor(value)) * 100);
+        var frac = f < 10 ? "0" + f : "" + f;
+        switch (this.locale) {
+            case 'de':
+                return this.formatDecimalNumber(value, '.') + ',' + frac + ' ' + currencySymbol;
+            default:
+                return currencySymbol + this.formatDecimalNumber(value, ',') + '.' + frac;
         }
     };
 
