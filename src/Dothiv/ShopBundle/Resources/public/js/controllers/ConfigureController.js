d7e0010fef2520f9270028c431a6bc8d0dc2c53f
Markus Tacker <m@coderbyheart.de>
2014-12-16 20:35:49 +0100

    feat(shop): implement complete path

---
+++ src/Dothiv/ShopBundle/Resources/public/js/controllers/ConfigureController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('dotHIVApp.controllers').controller('ConfigureController', ['$scope', 'OrderModel', '$state', '$stateParams', function ($scope, OrderModel, $state, $stateParams) {
+    if (OrderModel.isDone()) {
+        $state.transitionTo('lookupform', {"locale": $stateParams.locale});
+    }
+    $scope.order = OrderModel;
+    $scope.domain = $stateParams.domain;
+    $scope.redirectExample = "http://www." + $scope.domain + ".com/";
+    $scope.submit = function () {
+        $state.transitionTo('checkout', {"locale": $stateParams.locale, "domain": $scope.domain})
+    };
+}]);
