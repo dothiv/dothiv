3148680b9a89cee98edaa145a9858fde064b2e30
Markus Tacker <m@coderbyheart.de>
2014-12-19 09:19:30 +0100

    feat(shop): charge cards

--- src/Dothiv/ShopBundle/Command/ChargeOrdersCommand.php
+++ src/Dothiv/ShopBundle/Command/ChargeOrdersCommand.php
@@ -2,11 +2,13 @@
 
 namespace Dothiv\ShopBundle\Command;
 
+use Dothiv\BusinessBundle\Entity\Invoice;
 use Dothiv\ShopBundle\Entity\Order;
 use Dothiv\ShopBundle\Repository\OrderRepositoryInterface;
-use Dothiv\ValueObject\EmailValue;
+use Dothiv\ShopBundle\Service\InvoiceServiceInterface;
 use Dothiv\ValueObject\IdentValue;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\TableHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -23,28 +25,54 @@ class ChargeOrdersCommand extends ContainerAwareCommand
     {
         /** @var OrderRepositoryInterface $orderRepo */
         $orderRepo = $this->getContainer()->get('dothiv.repository.shop_order');
+        /** @var InvoiceServiceInterface $invoiceService */
+        $invoiceService = $this->getContainer()->get('dothiv.shop.invoice');
 
         \Stripe::setApiKey($this->getContainer()->getParameter('stripe_secret_key'));
 
         foreach ($orderRepo->findNew() as $order) {
+            $invoice = $invoiceService->createInvoice($order);
             /** @var Order $order */
-            // TODO: persist order currency
-            // TODO: extract pricing service
-            /*
             $charge = \Stripe_Charge::create(array(
                 'amount'      => $invoice->getTotalPrice(),
-                'currency'    => 'eur',
-                'card'        => $order->getStripeCard()->toScalar(),
+                'currency'    => strtolower($order->getCurrency()->toScalar()),
+                'card'        => $order->getStripeToken()->toScalar(),
                 'description' => $invoice->getItemDescription()
             ));
             $order->setStripeCharge(new IdentValue($charge->id));
             $orderRepo->persist($order)->flush();
             $output->writeln(
-                sprintf('Processed order by %s.', $order->getEmail())
+                sprintf('Processed order for %s by %s.', $order->getDomain(), $order->getEmail())
             );
-            */
-
-
+            $this->showOrder($output, $order, $invoice);
         }
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param Order           $order
+     */
+    protected function showOrder(OutputInterface $output, Order $order, Invoice $invoice)
+    {
+        $table = new TableHelper();
+        $table->setHeaders(array('Name', 'Value'));
+        $table->addRow(array('Domain', $order->getDomain()->toScalar()));
+        $table->addRow(array('Price', ($invoice->getTotalPrice() / 100) . ' ' . ($order->getCurrency() == Order::CURRENCY_EUR ? '€' : '$')));
+        $table->addRow(array('Name', $order->getFirstname() . ' ' . $order->getLastname()));
+        $table->addRow(array('Email', $order->getEmail()));
+        $table->addRow(array('Locality', $order->getLocality()));
+        if ($order->getLocality2()->isDefined()) {
+            $table->addRow(array('Locality (ctd.)', $order->getLocality2()->get()));
+        }
+        $table->addRow(array('City', $order->getCity()));
+        $table->addRow(array('Country', $order->getCountry()));
+        if ($order->getOrganization()->isDefined()) {
+            $table->addRow(array('Organization', $order->getOrganization()->get()));
+        }
+        $table->addRow(array('Phone', $order->getPhone()));
+        if ($order->getFax()->isDefined()) {
+            $table->addRow(array('Fax', $order->getFax()->get()));
+        }
+        $table->render($output);
     }
 }
